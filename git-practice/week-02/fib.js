af28e5001fdfebd49fda966df224a75a075e7253
jack880311 <tpdcjo0311@gmail.com>
2024-09-23 17:42:33 +0800

    完成week-02的fib.js：完成費氏數列函數fib()的實作。 (02b00)

--- git-practice/week-02/fib.js
+++ git-practice/week-02/fib.js
@@ -2,9 +2,9 @@ function fib(n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
     else return fib(n-1) + fib(n-2);
-}
+} //利用遞迴輸出所求費氏數之前兩個費氏數的總和
 
-console.log(fib(0));
-console.log(fib(1));
-console.log(fib(5));
-console.log(fib(10));
+console.log(fib(0)); //輸出fib(0)的結果，確認為0
+console.log(fib(1)); //輸出fib(1)的結果，確認為1
+console.log(fib(5)); //輸出fib(5)的結果，確認為5
+console.log(fib(10)); //輸出fib(10)的結果，確認為55
